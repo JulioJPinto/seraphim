5f4fc3f65e0690b90b59aea7b8b59c2e18fa8a74
Júlio Pinto <pinto.julioj03@gmail.com>
2024-08-20 01:03:00 +0100

    feat: photo, about me and social media

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const data = [
     {
-        organization: "CeSIUM - Centro de Estudantes de Engenharia Informática da Universidade do Minho",
+        organization: "CeSIUM",
         positions: [
             "Marketing Director",
             "Marketing Co-Director",
             "Collaborator"
-        ]
+        ],
+        link: "https://cesium.di.uminho.pt"
     },
     {
         organization: "CoderDojo Braga",
         positions: [
             "Co-Champion & Mentor",
             "Mentor"
-        ]
+        ],
+        link: "https://coderdojobraga.org"
     },
     {
         organization: "Martins & Filhos, SA",
@@ -33,39 +35,31 @@ function createTimeline(data) {
 
     data.forEach(item => {
 
-        const organizationDiv = document.createElement('div');
-        organizationDiv.className = 'flex flex-col gap-4';
+        const organizationDiv = document.createElement('organization-div');
+        organizationDiv.className = 'flex flex-col gap-2';
 
-        const line = document.createElement('div');
-        line.className = 'line h-full w-3 bg-black-800 z-40';
-
-        organizationDiv.appendChild(line);
-    
         item.positions.forEach((position, index) => {
             const timelineItem = document.createElement('div');
             timelineItem.className = 'timeline-item';
     
             const bullet = document.createElement('div');
-            bullet.className = 'bullet';
-            if (index === 0) {
-                bullet.style.width = '16px';
-                bullet.style.height = '16px';
-            }
-    
+            bullet.className = index === 0 ? 'bullet-first' : 'bullet';
+
             bullet.style.marginLeft = index === 0 ? '0' : '2px';
-    
+
             const content = document.createElement('div');
             content.className = 'content';
     
             if (index === 0) {
-                const title = document.createElement('div');
-                title.className = 'title kulim-park-regular';
+                const title = document.createElement('a');
+                title.className = 'title kulim-park-semibold text-xl text-text';
                 title.textContent = item.organization;
+                title.href = item.link;
                 content.appendChild(title);
             }
     
             const positionElem = document.createElement('div');
-            positionElem.className = 'position kulim-park-regular';
+            positionElem.className = 'position kulim-park-regular text-md text-text';
             positionElem.textContent = position;
     
             content.appendChild(positionElem);
@@ -76,11 +70,73 @@ function createTimeline(data) {
             organizationDiv.appendChild(timelineItem);
         });
 
-;
-
     
         timeline.appendChild(organizationDiv);
     });    
 }
 
 createTimeline(data);
+
+/*
+const profileData = {
+    "contacts": [
+        {
+            "platform": "email",
+            "url": "",
+            "icon": "email",
+        },
+        {
+            "platform": "Twitter",
+            "url": "https://twitter.com/username",
+            "icon": "twitter"
+        },
+        {
+            "platform": "LinkedIn",
+            "url": "https://www.linkedin.com/in/username",
+            "icon": "linkedin"
+        },
+        {
+            "platform": "GitHub",
+            "url": "https://github.com/username",
+            "icon": "code"
+        }
+    ]
+};
+
+function generateContactSection(profileData) {
+    const contactSection = document.getElementById('contactinfo');
+    contactSection.className = 'bg-secondary w-full rounded-2xl px-8 py-4 mt-auto';
+
+    
+    const iconContainer = document.createElement('div');
+    iconContainer.className = 'flex gap-6 space-x-6';
+    
+    profileData.contacts.forEach(contact => {
+        
+        if (contact.platform === 'email' && !contact.url) {
+            contact.url = `mailto:me@juliojpinto.com`; 
+        }
+
+        const link = document.createElement('a');
+        link.href = contact.url;
+        link.target = "_blank"; 
+
+        const icon = document.createElement('img');
+        icon.src = `path/to/icons/${contact.icon}.png`; 
+        icon.alt = `${contact.platform}`;
+        icon.className = 'h-8 w-8';
+
+        
+        link.appendChild(icon);
+
+        
+        iconContainer.appendChild(link);
+    });
+
+    contactSection.appendChild(iconContainer);
+
+    return contactSection;
+}
+
+generateContactSection(profileData);
+*/
